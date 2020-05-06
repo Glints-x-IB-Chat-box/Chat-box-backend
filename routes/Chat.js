5d1df85397fb492908535373711249dd42f56c09
riverhuang-batam <riverhuang011@gmail.com>
2020-05-06 12:46:51 +0700

    fixing multers

--- routes/Chat.js
+++ routes/Chat.js
@@ -4,24 +4,16 @@ const router = express.Router()
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-    destinantion:(req, file, cb) => {
+    destination:(req, file, cb) => {
         cb(null, './public/chatImage/')
     },
     filename: (req, file, cb) => {
         cb(null, new Date().toISOString() + '-' + file.originalname)
     }
 })
-const fileFilter = (req, file, cb) => {
-    if(file.mimetype === 'image/jpg' || file.mimetype === 'image/png'){
-        cb(null, true)
-    } else {
-        cb(null, false);
-    }
-}
 const upload = multer({storage:storage, limits:{
     fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
+    }
 })
 router.get('/getchat', ChatController.getChat)
 router.post('/postchat', upload.single('imageUrl'),ChatController.postChat)
